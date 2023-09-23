0719f7792b8b36ac337c64fffd0cd0f157fd383f
Nicholas J Hill <nicholasjhill@live.co.uk>
2023-09-23 15:35:45 +0100

    Add scroll bar to Page

--- src/components/page/Page.tsx
+++ src/components/page/Page.tsx
@@ -1,21 +1,39 @@
-import { Grid } from "@mui/material";
+import { Grid, useTheme } from "@mui/material";
+import { motion, useScroll } from "framer-motion";
 import { PropsWithChildren } from "react";
 import { Footer } from "../footer/Footer";
 
 function Page({ children }: PropsWithChildren) {
+  const { scrollYProgress } = useScroll();
+  const theme = useTheme();
+
   return (
-    <Grid container minHeight="100vh" alignContent="space-between">
-      <Grid container spacing={2} p={2}>
-        <Grid item xs={12}>
-          {children}
+    <>
+      <motion.div
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          right: 0,
+          height: "5px",
+          background: theme.palette.primary.main,
+          transformOrigin: "0%",
+          scaleX: scrollYProgress,
+        }}
+      />
+      <Grid container minHeight="100vh" alignContent="space-between">
+        <Grid container spacing={2} p={2}>
+          <Grid item xs={12}>
+            {children}
+          </Grid>
+        </Grid>
+        <Grid container alignContent="end">
+          <Grid item xs={12}>
+            <Footer />
+          </Grid>
         </Grid>
       </Grid>
-      <Grid container alignContent="end">
-        <Grid item xs={12}>
-          <Footer />
-        </Grid>
-      </Grid>
-    </Grid>
+    </>
   );
 }
 
