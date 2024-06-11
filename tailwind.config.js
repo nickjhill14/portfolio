43b3e41881a8ea57458d55e6bf27d093ab813752
Nicholas Hill <nicholasjhill@live.co.uk>
2024-06-11 22:47:40 +0100

    Bump NextUI deps

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 import { nextui } from "@nextui-org/react";
 
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {}
   },
   darkMode: "class",
-  plugins: [nextui()],
+  plugins: [nextui()]
 };
