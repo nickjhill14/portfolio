a3058a96233015bf37d536ff742751800912be9f
Nicholas Hill <nicholasjhill@live.co.uk>
2024-06-18 22:57:52 +0100

    Remove dark mode

--- src/components/footer/Footer.test.tsx
+++ src/components/footer/Footer.test.tsx
@@ -1,47 +1,7 @@
 import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 import { Footer } from "./Footer";
 
 describe(Footer, () => {
-  it("changes the colour mode when clicking the colour mode icon", async () => {
-    render(<Footer />);
-    await userEvent.click(
-      screen.getByRole("button", { name: "Toggle light mode" }),
-    );
-
-    expect(
-      screen.getByRole("button", { name: "Toggle dark mode" }),
-    ).toBeInTheDocument();
-    expect(
-      screen.queryByRole("button", { name: "Toggle light mode" }),
-    ).not.toBeInTheDocument();
-  });
-
-  it("changes the colour mode back clicking the colour mode icon twice", async () => {
-    render(<Footer />);
-    await userEvent.click(
-      screen.getByRole("button", { name: "Toggle light mode" }),
-    );
-    await userEvent.click(
-      screen.getByRole("button", { name: "Toggle dark mode" }),
-    );
-
-    expect(
-      screen.getByRole("button", { name: "Toggle light mode" }),
-    ).toBeInTheDocument();
-    expect(
-      screen.queryByRole("button", { name: "Toggle dark mode" }),
-    ).not.toBeInTheDocument();
-  });
-
-  it("does not render the hidden acknowledgment by default", async () => {
-    render(<Footer />);
-
-    expect(
-      screen.queryByText("Thank you to the best pair, Kate"),
-    ).not.toBeInTheDocument();
-  });
-
   it("renders a link to the GH repo", async () => {
     render(<Footer />);
     const visitRepoLink = screen.getByRole("button", { name: "Visit repo" });
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,38 +1,15 @@
-import DarkModeIcon from "@mui/icons-material/DarkMode";
 import GitHubIcon from "@mui/icons-material/GitHub";
-import LightModeIcon from "@mui/icons-material/LightMode";
 import { Button, Link } from "@nextui-org/react";
-import { useState } from "react";
-import { useColourMode } from "../../contexts/themeContext/ThemeContext";
 
-export const Footer = () => {
-  const { toggleColourMode } = useColourMode();
-
-  const [lightMode, setLightMode] = useState(false);
-
-  const handleToggleColourMode = () => {
-    setLightMode(!lightMode);
-    toggleColourMode();
-  };
-
-  return (
-    <footer className="flex w-full justify-between p-2" data-testid="footer">
-      {/* TODO: Fix */}
-      <Button
-        onClick={handleToggleColourMode}
-        isIconOnly
-        aria-label={lightMode ? "Toggle dark mode" : "Toggle light mode"}
-      >
-        {lightMode ? <DarkModeIcon /> : <LightModeIcon />}
-      </Button>
-      <Button
-        as={Link}
-        aria-label="Visit repo"
-        href="https://github.com/nickjhill14/portfolio"
-        isExternal
-      >
-        <GitHubIcon />
-      </Button>
-    </footer>
-  );
-};
+export const Footer = () => (
+  <footer className="flex w-full justify-between p-2" data-testid="footer">
+    <Button
+      as={Link}
+      aria-label="Visit repo"
+      href="https://github.com/nickjhill14/portfolio"
+      isExternal
+    >
+      <GitHubIcon />
+    </Button>
+  </footer>
+);
