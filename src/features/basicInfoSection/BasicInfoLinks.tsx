33bb1fce4c1481a819599bc48ff226ae194aaee1
Nicholas Hill <nicholasjhill@live.co.uk>
2024-06-24 10:39:25 +0100

    Add icons to BasicInfoLinks

--- src/features/basicInfoSection/BasicInfoLinks.tsx
+++ src/features/basicInfoSection/BasicInfoLinks.tsx
@@ -1,3 +1,6 @@
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import MailIcon from "@mui/icons-material/Mail";
 import { Link } from "@nextui-org/link";
 
 type BasicInfoLinksProps = {
@@ -12,24 +15,38 @@ export const BasicInfoLinks = ({
   gitHub,
 }: BasicInfoLinksProps) => (
   <div className="flex flex-col gap-4 md:flex-row">
-    <Link href={`mailto:${email}`} isExternal underline="hover" showAnchorIcon>
-      {email}
-    </Link>
-    <Link
-      href={`https://www.linkedin.com/in/${linkedIn}`}
-      isExternal
-      underline="hover"
-      showAnchorIcon
-    >
-      {linkedIn}
-    </Link>
-    <Link
-      href={`https://www.github.com/${gitHub}`}
-      underline="hover"
-      isExternal
-      showAnchorIcon
-    >
-      {gitHub}
-    </Link>
+    <div className="flex align-middle gap-1">
+      <MailIcon />
+      <Link
+        href={`mailto:${email}`}
+        isExternal
+        underline="hover"
+        showAnchorIcon
+      >
+        {email}
+      </Link>
+    </div>
+    <div className="flex align-middle gap-1">
+      <LinkedInIcon />
+      <Link
+        href={`https://www.linkedin.com/in/${linkedIn}`}
+        isExternal
+        underline="hover"
+        showAnchorIcon
+      >
+        {linkedIn}
+      </Link>
+    </div>
+    <div className="flex align-middle gap-1">
+      <GitHubIcon />
+      <Link
+        href={`https://www.github.com/${gitHub}`}
+        underline="hover"
+        isExternal
+        showAnchorIcon
+      >
+        {gitHub}
+      </Link>
+    </div>
   </div>
 );
