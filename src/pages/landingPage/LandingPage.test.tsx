fe02fb188b49650be60e987489bf43447f3cd882
Nicholas Hill <nicholasjhill@live.co.uk>
2024-06-25 01:09:19 +0100

    Add dog gif

--- src/pages/landingPage/LandingPage.test.tsx
+++ src/pages/landingPage/LandingPage.test.tsx
@@ -1,11 +1,14 @@
-import { screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import { act } from "react-dom/test-utils";
+import { afterEach } from "vitest";
 import { basicInfo } from "../../config/basicInfo";
 import { BasicInfoSection } from "../../features/basicInfoSection/BasicInfoSection";
+import { LandingPageLinks } from "../../features/landingPageLinks/LandingPageLinks";
 import { getProps, mockComponent } from "../../utils/ComponentMocks";
-import { renderWithMemoryRouter } from "../../utils/Renderers";
 import { LandingPage } from "./LandingPage";
 
 vitest.mock("../../features/basicInfoSection/BasicInfoSection");
+vitest.mock("../../features/landingPageLinks/LandingPageLinks");
 vitest.mock("react-router-dom", async () => ({
   ...(await vitest.importActual<typeof import("react-router-dom")>(
     "react-router-dom",
@@ -16,14 +19,41 @@ vitest.mock("react-router-dom", async () => ({
 describe(LandingPage, () => {
   beforeEach(() => {
     mockComponent(BasicInfoSection);
+    mockComponent(LandingPageLinks);
+    vitest.useFakeTimers({ shouldAdvanceTime: true });
   });
 
-  it("renders the page when loading has completed", async () => {
-    renderWithMemoryRouter(<LandingPage />);
+  afterEach(() => {
+    vi.runOnlyPendingTimers();
+    vi.useRealTimers();
+  });
+
+  it("renders the page", () => {
+    render(<LandingPage />);
 
     expect(getProps(BasicInfoSection).lastCall.basicInfo).toStrictEqual(
       basicInfo,
     );
-    expect(screen.getByTestId("landing-page-links")).toBeInTheDocument();
+    expect(screen.getByText(LandingPageLinks.name)).toBeInTheDocument();
+    expect(
+      screen.getByRole("img", { name: "Jumping dog" }),
+    ).toBeInTheDocument();
+    expect(
+      screen.queryByRole("heading", { name: "👋 Welcome!", level: 2 }),
+    ).not.toBeInTheDocument();
+  });
+
+  it.todo("renders the welcome message after 4.5 secs", async () => {
+    render(<LandingPage />);
+    act(() => {
+      vi.advanceTimersByTime(4500);
+    });
+
+    expect(
+      screen.getByRole("heading", { name: "👋 Welcome!", level: 2 }),
+    ).toBeInTheDocument();
+    expect(
+      screen.queryByRole("img", { name: "Jumping dog" }),
+    ).not.toBeInTheDocument();
   });
 });
