c12e0bf8aae9be4946a1b06f6c453e6f029dbb31
Nicholas Hill <nicholasjhill@live.co.uk>
2024-07-03 10:00:13 +0100

    Add BlogPage animation

--- src/pages/blogPage/BlogPage.tsx
+++ src/pages/blogPage/BlogPage.tsx
@@ -1,5 +1,6 @@
 import { Biotech, Home } from "@mui/icons-material";
 import { Button } from "@nextui-org/react";
+import { motion } from "framer-motion";
 import { useNavigate } from "react-router-dom";
 import { Page } from "../../components/page/Page";
 import {
@@ -21,19 +22,26 @@ export const BlogPage = () => {
         Go Home
       </Button>
       <h1 className="text-4xl">Blog</h1>
-      <h2 className="text-2l text-secondary font-bold">Testing</h2>
-      <Button
-        startContent={<Biotech />}
-        onPress={() =>
-          navigate(
-            `${PortfolioRoutePaths.BLOG}${BlogRoutePaths.REACT_COMPONENT_TESTING}`,
-          )
-        }
-        color="primary"
-        className="self-start"
+      <motion.div
+        initial={{ opacity: 0 }}
+        transition={{ ease: "easeIn", duration: 0.5 }}
+        animate={{ opacity: 1 }}
+        className="flex flex-col gap-4"
       >
-        React Component Testing
-      </Button>
+        <h2 className="text-2l text-secondary font-bold">Testing</h2>
+        <Button
+          startContent={<Biotech />}
+          onPress={() =>
+            navigate(
+              `${PortfolioRoutePaths.BLOG}${BlogRoutePaths.REACT_COMPONENT_TESTING}`,
+            )
+          }
+          color="primary"
+          className="self-start"
+        >
+          React Component Testing
+        </Button>
+      </motion.div>
     </Page>
   );
 };
